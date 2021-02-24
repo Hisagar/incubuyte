8ba2df1966a57e674c7673d54c5c8a15ceb3b71e
Sagar Wankhade <32439113+Hisagar@users.noreply.github.com>
2021-02-24 13:47:07 +0530

    first step finished

--- AddString.java
+++ AddString.java
@@ -4,10 +4,29 @@ class AddString{
         Scanner in = new Scanner(System.in);
         String strNumber = in.nextLine();
         int no = add(strNumber);
+        System.out.println(no);
     }
     public static int add(String strNumber){
         int res=0;
-        
+        if(strNumber!=null){
+            String []splitedString = SplitString(strNumber);
+            res= sumOfString(splitedString);
+        }
        return res;
+    }
+    public static String[] SplitString(String strNumber){
+        return strNumber.split(",");
+    }
+    public static int sumOfString(String []splitedString){
+        int sum =0;
+        for(int i=0;i<splitedString.length;i++){
+            try{
+                int n = Integer.parseInt(splitedString[i]);
+                sum += n;
+            }catch(Exception e){
+                
+            }
+        }
+        return sum;
     }
 }
